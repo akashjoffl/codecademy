dded93f68b892bb619c8fc1175343ac4537872b1
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 03:13:40 +0530

    Looping through Arrays

--- untitled.js
+++ untitled.js
@@ -1,3 +1,5 @@
-for (let counter=5; counter<11; counter++) {
-  console.log(counter);
-}
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+// Write your code below
+for (let i = 0; i < vacationSpots.length; i++)
+    console.log('I would love to visit ' + vacationSpots[i])
