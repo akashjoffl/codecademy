41ea2473ed57173cab667d9e5e13db0d65c47687
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 03:08:14 +0530

    LOOPS The For Loop

--- untitled.js
+++ untitled.js
@@ -1,10 +1,3 @@
-let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
-
-secretMessage.pop();
-secretMessage.push('to', 'Program');
-secretMessage[7]='right';
-secretMessage.shift();
-secretMessage.unshift('Programming');
-secretMessage.splice(5, 9, 'know'); // Use an array method to remove the strings get, right, the, first, time,, and replace them with the single string know,.
-console.log(secretMessage.join());
-console.log(2, secretMessage);
+for (let counter=5; counter<11; counter++) {
+  console.log(counter);
+}
