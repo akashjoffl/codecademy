97c9f1a8c751e6b2987eeb789fb4004afc98b502
Akash Jeganath <akashjoffl@icloud.com>
2020-04-11 03:24:40 +0530

    Nested Loops

--- untitled.js
+++ untitled.js
@@ -1,5 +1,11 @@
-const vacationSpots = ['Bali', 'Paris', 'Tulum'];
-
 // Write your code below
-for (let i = 0; i < vacationSpots.length; i++)
-    console.log('I would love to visit ' + vacationSpots[i])
+let bobsFollowers = ['Joe', 'Jill', 'Jack', 'Phil'];
+let tinasFollowers = ['Mike', 'Jack', 'Phil'];
+let mutualFollowers = [];
+for (let i = 0; i < bobsFollowers.length; i++) {
+    for (let j = 0; j < tinasFollowers.length; j++) {
+        if (bobsFollowers[i] === tinasFollowers[j]) {
+            console.log(mutualFollowers.push(bobsFollowers[i]))
+        }
+    }
+};
