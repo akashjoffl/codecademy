fd994b1fb897862e93019858add956167df9214e
Akash Jeganath <akashjoffl@icloud.com>
2020-04-12 02:21:52 +0530

    LOOPS The break Keyword

--- untitled.js
+++ untitled.js
@@ -1,11 +1,10 @@
-// Write your code below
-let bobsFollowers = ['Joe', 'Jill', 'Jack', 'Phil'];
-let tinasFollowers = ['Mike', 'Jack', 'Phil'];
-let mutualFollowers = [];
-for (let i = 0; i < bobsFollowers.length; i++) {
-    for (let j = 0; j < tinasFollowers.length; j++) {
-        if (bobsFollowers[i] === tinasFollowers[j]) {
-            console.log(mutualFollowers.push(bobsFollowers[i]))
-        }
-    }
-};
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+let currentCard;
+while (currentCard != 'spade') {
+    currentCard = cards[Math.floor(Math.random() * 4)];
+    console.log(currentCard);
+}
+
+
+
