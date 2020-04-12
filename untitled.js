a8d40fc6292dd2c437140152f15d97b3d46c78be
Akash Jeganath <akashjoffl@icloud.com>
2020-04-13 01:09:32 +0530

    The .forEach() Method

--- untitled.js
+++ untitled.js
@@ -1,3 +1,4 @@
-.forEach()
-    .map()
-    .filter()
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
