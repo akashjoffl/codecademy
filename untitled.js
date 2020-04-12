32cb3cc919045d58f10189b39b57eb5fe6d6e317
Akash Jeganath <akashjoffl@icloud.com>
2020-04-13 01:13:16 +0530

    The .map() Method

--- untitled.js
+++ untitled.js
@@ -1,4 +1,13 @@
-const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+const animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Iterate over fruits below
-fruits.forEach(fruit => console.log(`I want to eat a ${fruit}.`))
+// Create the secretMessage array below
+const secretMessage = animals.map(animal => animal[0]);
+console.log(secretMessage)
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers = bigNumbers.map(num => num / 100);
+
+console.log(smallNumbers)
