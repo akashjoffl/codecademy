d781d0f69b1dedd2c49056508d9ad43d54bcac7b
Akash Jeganath <akashjoffl@icloud.com>
2020-04-15 04:50:02 +0530

    ITERATORS Iterator Documentation

--- untitled.js
+++ untitled.js
@@ -1,10 +1,15 @@
-const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
 
-const foundAnimal = animals.findIndex(animal => {
-    return animal === 'elephant';
-});
+// Something is missing in the method call below
 
-const startsWithS = animals.findIndex(animal => {
-    return animal[0] === 's' ? true : false;
-});
+console.log(words.some(word => {
+    return word.length < 6;
+}));
 
+// Use filter to create a new array
+const interestingWords = words.filter((word) => { return word.length > 5 });
+
+
+// Make sure to uncomment the code below and fix the incorrect code before running it
+
+console.log(interestingWords.every((word) => { return word.length > 5 }));
