3d489300c42119914de3c98204c59f8c4ca7305d
Akash Jeganath <akashjoffl@icloud.com>
2020-04-17 00:49:09 +0530

    ADVANCED OBJECTS Privacy

--- untitled.js
+++ untitled.js
@@ -1,8 +1,11 @@
 const robot = {
-    energyLevel: 100,
-    checkEnergy() {
-        console.log(`Energy is currently at ${this.energyLevel}%.`)
+    _energyLevel: 100,
+    recharge() {
+        this._energyLevel += 30;
+        console.log(`Recharged! Energy is currently at ${this._energyLevel}%.`)
     }
-}
+};
 
-robot.checkEnergy();
+robot._energyLevel = 'high'
+
+console.log(robot.recharge());
